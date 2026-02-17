cc7cfd256b4e47d7436facf44386c65ea4b83386
Eweh4 <edvin.hagard@gmail.com>
2026-02-17 16:01:42 +0100

    Changes by lucas

--- Shared/src/main/java/dat055/group5/export/User.java
+++ Shared/src/main/java/dat055/group5/export/User.java
@@ -1,24 +1,24 @@
 package dat055.group5.export;
 
 public class User {
-    String userName;
+    String username;
     String password;
 
     public User(String userName, String password) {
-        this.userName = userName;
+        this.username = userName;
         this.password = password;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String userName) {
+        this.username = userName;
     }
 
     public void setPassword(String password) {
@@ -30,7 +30,7 @@ public class User {
     }
 
     public void setUser(String userName, String password) {
-        this.userName = userName;
+        this.username = userName;
         this.password = password;
     }
 
