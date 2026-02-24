c5c84b06e84c1fd61cb6ac835d9ef03b0a6ff71b
aunique <coolatanwiss@gmail.com>
2026-02-24 21:32:24 +0100

    push

--- Client/src/main/java/dat055/group5/client/view/components/ChatListCell.java
+++ Client/src/main/java/dat055/group5/client/view/components/ChatListCell.java
@@ -1,10 +1,17 @@
 package dat055.group5.client.view.components;
 
+import dat055.group5.client.controller.ChatMessageController;
 import dat055.group5.export.Channel;
 import dat055.group5.export.Message;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListCell;
 
+import java.io.IOException;
+
 public class ChatListCell extends ListCell<Message> {
+
+    private FXMLLoader loader;
+    private ChatMessageController chatMessageController;
 
     //setup javafx components
 
@@ -17,7 +24,26 @@ public class ChatListCell extends ListCell<Message> {
     protected void updateItem(Message message, boolean empty){
         super.updateItem(message, empty);
 
-        // use components to draw stuff
+        if (empty || message == null) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            if (loader == null) {
+                // Load your FXML design
+                loader = new FXMLLoader(getClass().getResource("/path/to/chat_bubble.fxml"));
+                try {
+                    loader.load();
+                    chatMessageController = loader.getController();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Pass the data to your FXML's controller
+            chatMessageController.updateUI(message);
+
+            setGraphic(chatMessageController.getParent());
+        }
 
     }
 
